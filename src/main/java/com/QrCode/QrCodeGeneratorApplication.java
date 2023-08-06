53c9f31536dc81523b3055fcb439970453c4624e
Udipta <dasdsa1208@gmail.com>
2023-08-06 17:17:39 +0530

    commandline chnages

--- src/main/java/com/QrCode/QrCodeGeneratorApplication.java
+++ src/main/java/com/QrCode/QrCodeGeneratorApplication.java
@@ -1,13 +1,28 @@
 package com.QrCode;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.QrCode.Service.OpentelemetryService;
+
 @SpringBootApplication
-public class QrCodeGeneratorApplication {
+public class QrCodeGeneratorApplication implements CommandLineRunner{
+	
+	
+	@Autowired
+	private OpentelemetryService opentelemetryService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(QrCodeGeneratorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		opentelemetryService.getResponseService();
+		System.out.println("VOILD IT'S WORKING");
+		
 	}
 
 }
